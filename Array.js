1fdce68c7c5f2efea5287c81bd4fefe73cd94f3a
Priyanka Patel <55329362+priyankapatel1234@users.noreply.github.com>
2020-09-20 16:49:39 +0530

    Array.js

---
+++ Array.js
@@ -0,0 +1,48 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    var array=nums.sort(function(a,b){
+        return(a-b);
+    });
+    var unique_array = array.filter(function(elem, index, self) {
+        return index === self.indexOf(elem);
+    })
+
+    return unique_array[unique_array.length - 2];
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
